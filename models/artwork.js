28b7447169dd73d147fded0f49c4ebdf1414f6f0
Jason Nguyen <j1.nguyen@yahoo.com>
2024-05-17 10:51:19 -0700

    create artwork and comment models

---
+++ models/artwork.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+}, {
+    timestamps: true
+});
+
+const artworkSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    artType: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    image: {
+        url: {
+            type: String,
+            required: true
+        }
+    },
+    comments: [commentSchema]
+}, {
+    timestamps: true
+});
