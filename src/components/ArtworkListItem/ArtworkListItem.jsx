646caeb0ef6264aae82bedf4f63a576cf22bc84c
Jason Nguyen <j1.nguyen@yahoo.com>
2024-05-23 11:33:07 -0700

    formatted date display on artworks

--- src/components/ArtworkListItem/ArtworkListItem.jsx
+++ src/components/ArtworkListItem/ArtworkListItem.jsx
@@ -2,15 +2,20 @@ import './ArtworkListItem.css'
 import { Link } from 'react-router-dom';
 
 export default function ArtworkListItem({ artworkItem }) {
+    const formattedDate = new Date(artworkItem.date).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+    });
     return (
         <Link to={artworkItem._id} className="artwork-link">
             <div className='artwork'>
                 <img src={artworkItem.image.url} alt="artwork" />
                 <div className='artwork-card-details'>
-                    <h2>{artworkItem.title}</h2>
-                    <p>{artworkItem.user.name}</p>
-                    <p>{artworkItem.artType}</p>
-                    <p>{artworkItem.date}</p>
+                    <h2>Title: {artworkItem.title}</h2>
+                    <p>Artist: {artworkItem.user.name}</p>
+                    <p>Art Type: {artworkItem.artType}</p>
+                    <p>Date: {formattedDate}</p>
                 </div>
             </div>
         </Link>
