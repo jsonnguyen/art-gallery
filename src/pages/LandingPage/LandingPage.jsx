d11fa543bb89d6bab45ac022795bfef9d2ed7094
uyamini <uyamini1996@gmail.com>
2024-05-24 09:26:12 -0500

    style landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,15 +1,18 @@
 // src/pages/LandingPage/LandingPage.jsx
 import React from 'react';
 import './LandingPage.css';
+import backgroundImage from '/Users/yamini/code/art-gallery/src/background.jpg'; 
 
 export default function LandingPage({ user, setUser }) {
   return (
     <div className="LandingPage">
       <main>
         <section className="hero">
-          <h1>Welcome to the Virtual Art Gallery Tour</h1>
-          <p>Create and explore virtual art galleries. Upload your artwork, curate personal galleries, and share them with the community.</p>
-          {!user && <a href="/signup" className="cta-button">Get Started</a>}
+          <div className="hero-content">
+            <h1>Welcome to the Virtual Art Gallery Tour</h1>
+            <p>Create and explore virtual art galleries. Upload your artwork, curate personal galleries, and share them with the community.</p>
+            {!user && <a href="/signup" className="cta-button">Get Started</a>}
+          </div>
         </section>
       </main>
     </div>
